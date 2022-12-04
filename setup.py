5922a83224c593322ecc81f4fe4d6b6c238c3543
Rohan <rohan.mathews1999@gmail.com>
2022-12-04 18:03:07 +0100

    Final commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Snow-World',
+      version='1.0',
+      description='Create Snowflakes',
+      author='Rohan Mathews',
+      author_email='rohan.mathews1999@gmail.com',
+      url='https://github.com/rohanmathews/Snow-World/',
+      packages=['numpy~=1.23.5', 'turtle'],
+     )
